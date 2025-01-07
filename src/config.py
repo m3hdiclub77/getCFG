e620e4bb33310d6305e993de75d3de3e7daf1157
m3hdiclub <132158452+m3hdiclub@users.noreply.github.com>
2025-01-07 09:03:49 +0330

    Update config.py

--- src/config.py
+++ src/config.py
@@ -47,10 +47,7 @@ class ProxyConfig:
     def __init__(self):
         # List of channels or URLs to fetch proxy configurations from
         self.SOURCE_URLS = [
-            ChannelConfig("https://t.me/s/v2ray_free_conf"),
-            ChannelConfig("https://t.me/s/PrivateVPNs"),
-            ChannelConfig("https://raw.githubusercontent.com/4n0nymou3/wg-config-fetcher/refs/heads/main/configs/wireguard_configs.txt"),
-            ChannelConfig("https://raw.githubusercontent.com/4n0nymou3/ss-config-updater/refs/heads/main/configs.txt")
+            ChannelConfig("https://raw.githubusercontent.com/m3hdiclub/free-server/main/mine")
         ]
 
         # Minimum and maximum number of configurations per protocol
