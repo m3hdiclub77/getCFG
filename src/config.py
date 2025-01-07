af245bc917ce90403756b661b78279c4f764c850
m3hdiclub <132158452+m3hdiclub@users.noreply.github.com>
2025-01-07 09:23:42 +0330

    Update config.py

--- src/config.py
+++ src/config.py
@@ -11,20 +11,18 @@ class ProxyConfig:
     def __init__(self):
         self.TELEGRAM_CHANNELS = [
             ChannelConfig("https://t.me/s/xs_filternet"),
-	    ChannelConfig("https://t.me/s/v2ray_free_conf"),
             ChannelConfig("https://t.me/s/v2rayvpno"),
-            ChannelConfig("https://t.me/s/ZibaNabz"),
             ChannelConfig("https://t.me/s/configV2rayForFree"),
             ChannelConfig("https://t.me/s/v2rayngvpn"),
-            ChannelConfig("https://t.me/s/SvnV2ray"), 
-            ChannelConfig("https://t.me/s/RadixVPN"),
-            ChannelConfig("https://t.me/s/PrivateVPNs"),
-            ChannelConfig("https://t.me/s/VlessConfig"),
-            ChannelConfig("https://t.me/s/freewireguard")
+            ChannelConfig("https://t.me/s/DeamNet_proxy"),
+            ChannelConfig("https://t.me/s/V2RFa"),
+            ChannelConfig("https://t.me/s/sinavm"),
+            ChannelConfig("https://t.me/s/v2ray1_ng")
         ]
         
         self.SUBSCRIPTION_URLS = [
-            "https://raw.githubusercontent.com/m3hdiclub/free-server/main/mine",
+            "https://raw.githubusercontent.com/m3hdiclub/free-server/main/Cloudflare_vless_trojan",
+			"https://little-sea-273d.7mehdinorouzi7.workers.dev/m3hdiclub?sub",
 		"https://raw.githubusercontent.com/tkamirparsa/V2rayy/refs/heads/main/Sub.text555"
             # Add more subscription URLs here
         ]
